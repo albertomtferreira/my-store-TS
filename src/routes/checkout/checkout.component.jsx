9769d3bdc7eff45acd0a6ab3d3efb7782c31c9a2
Alberto Ferreira <60177437+albertomtferreira@users.noreply.github.com>
2024-01-17 22:03:44 +0000

    TS - Lesson 225

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import PaymentForm from '../../components/payment-form/payment-form.component';
 
-import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector.js';
+import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
 import {CheckoutContainer,CheckoutHeader,HeaderBlock,Total} from './checkout.styles.jsx';
 
 
--- src/store/user/user.action.ts
+++ src/store/user/user.action.ts
@@ -1,5 +1,6 @@
-import {USER_ACTION_TYPES} from './user.types';
+import { User } from 'firebase/auth';
 
+import {USER_ACTION_TYPES} from './user.types';
 import { createAction, withMatcher, Action, ActionWithPayload } from '../../utils/reducer/reducer.utils';
 import { UserData, AdditionalInformation } from '../../utils/firebase/firebase.utils';
 
@@ -17,7 +18,7 @@ export type SignInFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_IN_FAILED, E
 
 export type SignUpStart = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_START, {email: string, password: string, displayName: string, phoneNumber: string}>;
 
-export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user: UserData, additionalDetails: AdditionalInformation}>;
+export type SignUpSuccess = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user: User, additionalDetails: AdditionalInformation}>;
 
 export type SignUpFailed = ActionWithPayload<USER_ACTION_TYPES.SIGN_UP_FAILED, Error>;
 
@@ -37,7 +38,7 @@ export const googleSignInStart = withMatcher(():GoogleSignInStart => createActio
 
 export const emailSignInStart = withMatcher((email: string, password: string):EmailSignInStart => createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START,{email, password})); //done
 
-export const signInSuccess = withMatcher((user: UserData):SignInSuccess => createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)); //done
+export const signInSuccess = withMatcher((user: UserData & {id: string}):SignInSuccess => createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)); //done
 
 export const signInFailed = withMatcher((error: Error):SignInFailed => createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error)); //done
 
@@ -45,7 +46,7 @@ export const signInFailed = withMatcher((error: Error):SignInFailed => createAct
 export const signUpStart = withMatcher((email: string, password: string, displayName: string, phoneNumber: string):SignUpStart => 
   createAction(USER_ACTION_TYPES.SIGN_UP_START, {email, password, displayName, phoneNumber}))
 
-export const signUpSuccess = withMatcher((user: UserData, additionalDetails: AdditionalInformation):SignUpSuccess => createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user, additionalDetails}))
+export const signUpSuccess = withMatcher((user: User, additionalDetails: AdditionalInformation):SignUpSuccess => createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, {user, additionalDetails}))
 
 export const signUpFailed = withMatcher((error: Error):SignUpFailed => createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error))
 
